04b9e3b40f7ae3c02e66bddce906a44b60d75026
Anton Abramov <antonabramov2000@gmail.com>
2021-12-19 23:55:18 +0700

    add balance in admin

--- src/apps/user/admin.py
+++ src/apps/user/admin.py
@@ -10,7 +10,7 @@ User = get_user_model()
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'balance')}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'banned'),
         }),
