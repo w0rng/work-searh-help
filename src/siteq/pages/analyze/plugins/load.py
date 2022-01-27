800d1836e5ff05001882e87603b512246ace017f
w0rng <antonabramov2000@gmail.com>
2022-01-27 11:30:47 +0700

    tehnhymnrtyjmtyu

--- src/siteq/pages/analyze/plugins/load.py
+++ src/siteq/pages/analyze/plugins/load.py
@@ -17,9 +17,5 @@ for path, _, files in walk('siteq/pages/analyze/plugins'):
         analyzers[file[:-3]] = module()
 
 
-print('------------')
-print(analyzers, flush=True)
-print('------------')
-
 modules = [f.module.pk for f in analyzers.values()]
 Module.objects.filter(~Q(pk__in=modules), type=ModuleType.ANALYZER).delete()
