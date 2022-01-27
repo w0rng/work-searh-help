b2060a6282a510ed1458a365e6b656199d6c6328
w0rng <antonabramov2000@gmail.com>
2022-01-27 13:59:11 +0700

    test horizontal views

--- src/siteq/pages/resume/forms.py
+++ src/siteq/pages/resume/forms.py
@@ -1,8 +1,26 @@
 from django import forms
-from apps.resume.models import Resume
+from apps.resume.models import Resume, Tag
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
 
 class ResumeForm(forms.ModelForm):
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple(
+            verbose_name='Теги',
+            is_stacked=False
+        )
+    )
+
     class Meta:
         model = Resume
         exclude = ['user']
+
+
+# Блок про API и способы взаимодействия в теорию
+# Как расчитываются баллы
+# Картинки кода заменить на листинг
+# Где-то в 2.2.3 написать формулу расчетов баллов
+# Написать ссылку в руководство пользователя
