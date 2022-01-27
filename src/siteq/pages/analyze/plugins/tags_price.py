5ab0543f48f5ec065848540c2687bca9c20e0b80
w0rng <antonabramov2000@gmail.com>
2022-01-27 12:02:50 +0700

    setbetyhetyijwtgt;piuw;fuq'werpgf

--- src/siteq/pages/analyze/plugins/tags_price.py
+++ src/siteq/pages/analyze/plugins/tags_price.py
@@ -1,6 +1,8 @@
 from apps.resume.models import Tag
 from django.db.models.aggregates import Avg
 from siteq.pages.analyze.plugins.base import BaseAnalyzer
+from django.db.models import IntegerField
+
 
 
 class Tags_PriceAnalyzer(BaseAnalyzer):
@@ -9,4 +11,4 @@ class Tags_PriceAnalyzer(BaseAnalyzer):
     description = 'Средняя зарплата по каждому тегу'
 
     def get_queryset(self, request):
-        return Tag.objects.all().annotate(count=Avg('vacancies__price')).filter(count__gt=2)
+        return Tag.objects.all().annotate(count=Avg('vacancies__price'), output_field=IntegerField()).filter(count__gt=2)
