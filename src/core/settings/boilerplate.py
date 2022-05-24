a2d17137e1d736f388cfe766e86ed49678c0f0e9
w0rng <antonabramov2000@gmail.com>
2022-05-24 09:26:26 +0700

    django smoke tests

--- src/core/settings/boilerplate.py
+++ src/core/settings/boilerplate.py
@@ -1,12 +1,11 @@
 import os.path
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
-TEST_RUNNER = 'apps.helpers.disable_test_command_runner.DisableTestCommandRunner'
+# TEST_RUNNER = 'apps.helpers.disable_test_command_runner.DisableTestCommandRunner'
--- src/core/settings/installed_apps.py
+++ src/core/settings/installed_apps.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     "django_filters",
     "bootstrap4",
     "easy_select2",
+    "django_smoke_tests",
     "apps.module",
     "apps.user",
     "pages",
